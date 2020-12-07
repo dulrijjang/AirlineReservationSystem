5cdf5e3e7234f3bfc4d5333607994e28055d526b
a_hsu <70989007+a_hsu@users.noreply.github.com>
2020-12-07 04:12:13 -0500

    3

--- JavaProject/src/edu/gsu/common/Action.java
+++ JavaProject/src/edu/gsu/common/Action.java
@@ -3,7 +3,7 @@ package edu.gsu.common;
 public class Action {
 	
 	public static final String LOGIN = "login";
-	public static final String GET_FLIGHTS = "getFlgihts";
+	public static final String GET_FLIGHTS = "getFlights";
 	public static final String REGISTER = "register";
 	
 
